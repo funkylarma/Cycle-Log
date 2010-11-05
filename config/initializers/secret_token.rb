340e7debbd6f031a6f7cb05cad205cd2ee92fd87
Adam Chamberlin <adam@stourvalleydesigns.com>
2010-11-05 19:39:58 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cyclelog::Application.config.secret_token = '8ff2c29ff766147136f53658896665b38c137829c2340b35215d3794ebffdc1629912457a544012ac9a7241ee5421c06d877124309d3af2b67ed0a35cb2db8aa'
